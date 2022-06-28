3fd32636917cff96a9e3df7bb288121dd24d57f7
vladponcea <vladponcea@icloud.com>
2022-06-28 09:37:10 +0300

    first commit

---
+++ TailSteps/TailSteps/TailStepsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TailStepsApp.swift
+//  TailSteps
+//
+//  Created by Vladut Mihai Poncea on 28.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct TailStepsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
